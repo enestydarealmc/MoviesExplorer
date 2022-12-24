c7a210afa85b15250645341a9b3a3b375406835d
Son Truong Nguyen <t.nguyen@innopolis.university>
2022-12-24 07:34:41 +0300

    hard-coded apikey

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -2,4 +2,5 @@ import {getConfig} from '@ijl/cli'
 
 export const server = 'https://tmdbproxy.herokuapp.com/3'
 export const image_server = 'https://image-tmdbproxy.herokuapp.com'
-export const API_KEY = getConfig()['apiKey']
+export const API_KEY = '71661be18f2fafefa4966ae143fa2251'
+// getConfig()['apiKey']
